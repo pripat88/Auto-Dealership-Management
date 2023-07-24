7348a96d7b6a1f67675d143ec32522c866544339
Priya <pxp405@case.edu>
2023-07-24 16:02:00 -0400

    Models.py done

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=100)
+    sold = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.vin
+
+
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.PositiveSmallIntegerField()
+
+    def get_api_url(self):
+        return reverse("api_sales", kwargs={"pk": self.pk})
+
+    def __str__(self) -> str:
+        return self.model_name
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+
+
+class Sale(models.Model):
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="Sale",
+        on_delete=models.CASCADE)
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="Sale",
+        on_delete=models.CASCADE)
+    customer = models.ForeignKey(
+        Customer,
+        related_name="Sale",
+        on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
