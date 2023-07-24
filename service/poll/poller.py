ed633bb160d2ea1f38d9667e84d34c084f089033
leahnp613 <leahnagarpowers@gmail.com>
2023-07-24 16:02:00 -0400

    migrated

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -11,23 +11,9 @@ django.setup()
 
 # Import models from service_rest, here. Ignore vs-code error hinting
 # from service_rest.models import Something
+from service_rest.models import AutomobileVO
 
 
-def poll(repeat=True):
-    while True:
-        print('Service poller polling for data')
-        try:
-            # Write your polling logic, here
-            # Do not copy entire file
-            pass
-        
-        except Exception as e:
-            print(e, file=sys.stderr)
-
-        if (not repeat):
-            break
-
-        time.sleep(60)
 
 
 if __name__ == "__main__":
