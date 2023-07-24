60058707a265a787ecbda4799f5af4e323a8b1a9
Priya <pxp405@case.edu>
2023-07-24 16:39:27 -0400

    poller.py done

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -11,19 +11,30 @@ django.setup()
 
 # Import models from sales_rest, here.
 # from sales_rest.models import Something
+from sales_rest.models import AutomobileVO
+
+def get_automobile():
+    response = requests.get("http://inventory-api:8000/api/automobiles/")
+    content = json.loads(response.content)
+    for automobile in content["autos"]:
+        AutomobileVO.objects.update_or_create(
+            color=automobile["color"],
+            year=automobile["year"],
+            vin=automobile["vin"],
+            model=automobile["model"]["name"],
+        )
+
 
 
 def poll(repeat=True):
     while True:
         print('Sales poller polling for data')
         try:
-            # Write your polling logic, here
-            # Do not copy entire file
+            get_automobile()
 
-            pass
         except Exception as e:
             print(e, file=sys.stderr)
-        
+
         if (not repeat):
             break
 
