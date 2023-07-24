61737a0546598f1c78adde5caacba13e65189e7d
leahnp613 <leahnagarpowers@gmail.com>
2023-07-24 16:40:19 -0400

    added polller logic

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -14,6 +14,35 @@ django.setup()
 from service_rest.models import AutomobileVO
 
 
+def get_automobile():
+    response = requests.get("http://inventory-api:8000/api/automobiles/")
+    content = json.loads(response.content)
+
+    for automobile in content["autos"]:
+        AutomobileVO.objects.update_or_create(
+            vin = automobile["vin"],
+            color = automobile["color"],
+            year = automobile ["year"],
+            model = automobile ["model"]["name"],
+        )
+
+
+def poll(repeat=True):
+    while True:
+        print('Service poller polling for data')
+        try:
+            # Write your polling logic, here
+            # Do not copy entire file
+
+            get_service()
+        
+        except Exception as e:
+            print(e, file=sys.stderr)
+
+        if (not repeat):
+            break
+
+        time.sleep(60)
 
 
 if __name__ == "__main__":
