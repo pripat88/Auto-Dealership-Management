ef5c67e8c3a8d410e07e1b81aeb937592a2fd563
leahnp613 <leahnagarpowers@gmail.com>
2023-07-26 14:56:34 -0400

    added inventory components

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,4 +1,7 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from 'react-router-dom';
+
+
+
 
 
 
@@ -7,18 +10,8 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-info">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          CarCar
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -123,7 +116,8 @@ function Nav() {
         </div>
       </div>
     </nav>
-  );
+  )
 }
 
 export default Nav;
+
