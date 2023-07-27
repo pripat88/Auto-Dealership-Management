54885de5be056638a659d94c274b3d230faa1523
leahnp613 <leahnagarpowers@gmail.com>
2023-07-27 17:14:32 -0400

    updated admin for service_rest

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Appointment, AutomobileVO, Technician
 
-# Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AutomobileVO)
+class AutomobileVO(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Technician)
+class Technician(admin.ModelAdmin):
+    pass
